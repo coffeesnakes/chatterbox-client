a77197698cdd49a4a8abcdcd936936afb1e8bf46
Jacob Berg <coffeesnakes@gmail.com>
2020-12-04 15:17:04 -0800

    ajax POST complete

--- client/scripts/parse.js
+++ client/scripts/parse.js
@@ -1,22 +1,33 @@
 var Parse = {
-
   server: `http://parse.${window.CAMPUS}.hackreactor.com/chatterbox/classes/messages`,
 
-  create: function(message, successCB, errorCB = null) {
-    // todo: save a message to the server
-  },
-
-  readAll: function(successCB, errorCB = null) {
+  create: function (message, successCB, errorCB = null) {
     $.ajax({
       url: Parse.server,
-      type: 'GET',
-      data: { order: '-createdAt' },
-      contentType: 'application/json',
+      type: "POST",
+      data: JSON.stringify(message),
+      contentType: "application/json",
       success: successCB,
-      error: errorCB || function(error) {
-        console.error('chatterbox: Failed to fetch messages', error);
-      }
+      error:
+        errorCB ||
+        function (data) {
+          console.error("chatterbox: Failed to send messages", data);
+        },
     });
-  }
+  },
 
+  readAll: function (successCB, errorCB = null) {
+    $.ajax({
+      url: Parse.server,
+      type: "GET",
+      data: { order: "-createdAt" },
+      contentType: "application/json",
+      success: successCB,
+      error:
+        errorCB ||
+        function (error) {
+          console.error("chatterbox: Failed to fetch messages", error);
+        },
+    });
+  },
 };
